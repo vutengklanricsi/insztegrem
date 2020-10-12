8959fe7e6740e86029ed936ea3c60e27c62c74db
vutengklanricsi <richard.vuongtanquang@gmail.com>
2020-10-12 12:58:59 +0200

    finishing the progressbar and adding document to firestore

--- src/comps/ProgressBar.js
+++ src/comps/ProgressBar.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useStorage from "../hooks/useStorage";
 
 const ProgressBar = ({ file, setFile }) => {
-	const { url, progress } = useStorage(file);
-	console.log(progress, url);
-  return <div className="progress-bar">progress</div>;
+  const { url, progress } = useStorage(file);
+  console.log(progress, url);
+  useEffect(() => {
+    if (url) {
+      setFile(null);
+    }
+  }, [url, setFile]);
+  return <div className="progress-bar" style={{ width: progress + "%" }}></div>;
 };
 
 export default ProgressBar;
